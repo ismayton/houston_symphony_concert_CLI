18d969d6adeaf6e72e0269744b33a9a08e33b1aa
Ian Mayton <ian.mayton@gmail.com>
2020-08-04 15:43:58 +0000

    first file migration with new filepaths

--- Gemfile
+++ Gemfile
@@ -1,4 +1,10 @@
 source "https://rubygems.org"
 
-# Specify your gem's dependencies in houston_symphony_concert_CLI.gemspec
+source "https://rubygems.org"
+
+gem 'rspec'
+gem 'pry'
+gem 'nokogiri'
+gem 'require_all'
+
 gemspec
---
+++ lib/piece.rb
@@ -0,0 +1,31 @@
+class Piece
+  attr_accessor :title, :composer
+  
+  @@all = []
+  
+  def initialize(title, composer = nil)
+    @title = title
+    if composer.class == Composer
+      @composer = composer
+      composer.add_piece(self)
+    end
+    save
+  end
+  
+  def composer=(composer)
+    if composer.class == Composer 
+      @composer = composer
+      if !composer.pieces.include?(self)
+        composer.pieces << self 
+      end
+    end
+  end 
+  
+  def save 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end
+end 

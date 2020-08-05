d7002f98d089448c8f9afe36ce2de529a74477c5
Ian Mayton <ian.mayton@gmail.com>
2020-08-05 16:58:57 +0000

    CLI methods

--- lib/houston_symphony_concert_CLI.rb
+++ lib/houston_symphony_concert_CLI.rb
@@ -1,4 +1,8 @@
 require "hs_concert/version"
+require 'bundler'
+Bundler.require
+
+require_all 'lib/hs_concert'
 
 module HoustonSymphonyConcertCLI
   class Error < StandardError; end
--- lib/hs_concert/CLI.rb
+++ lib/hs_concert/CLI.rb
@@ -1,6 +1,43 @@
 class HoustonSymphonyConcertCLI::CLI
+  attr_accessor :concert_url, :ordered
+  def initialize
+    @concert_url = "https://houstonsymphony.org/tickets/concerts/"
+    puts "Welcome to the Houston Symphony Concert CLI! Please choose an option below."
+    puts "1. View next program details."
+    puts "2. View next 4 concert dates."
+    puts "3. Search all concerts for composer by name"
+    
+    input = gets.to_i
+    
+    if input == 1 
+      puts "Option 1"
+      restart_or_quit
+    elsif input == 2 
+      first_four_concerts(@concert_url)
+      puts "Choose a concert for full details."
+      input = gets.to_i
+      display_full_details(input)
+      restart_or_quit
+    elsif input == 3 
+      puts "Option 3"
+      restart_or_quit
+    end
+    
+  end
   
-  def self.first_four_concerts(concert_url)
+  def restart_or_quit
+    puts "1. Return to Main Menu"
+    puts "2. Quit"
+    input = gets.to_i
+    if input == 1 
+      self.new
+    else
+      puts "Goodbye!"
+    end
+  end 
+      
+      
+  def first_four_concerts(concert_url)
     array = HoustonSymphonyConcertCLI::Scraper.scrape_concerts_page(concert_url)
     counter = 0
     @ordered = {}
@@ -12,7 +49,7 @@ class HoustonSymphonyConcertCLI::CLI
     end
   end
 
-  def self.display_full_details(input)
+  def display_full_details(input)
 
     returned = @ordered[input]
     puts "----------------------"
